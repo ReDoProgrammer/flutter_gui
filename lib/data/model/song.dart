7fcfb327d1206b9f236a449ad5f8dec73bc8944a
ReDo Programmer <redoprogrammer@gmail.com>
2025-08-09 13:57:10 +0700

    khai bao Song model va khai bao class Song

---
+++ lib/data/model/song.dart
@@ -0,0 +1,19 @@
+class Song {
+  String id;
+  String title;
+  String album;
+  String artist;
+  String source;
+  String image;
+  int duration;
+
+  Song({
+    required this.id,
+    required this.title,
+    required this.album,
+    required this.artist,
+    required this.source,
+    required this.image,
+    required this.duration,
+  });
+}
