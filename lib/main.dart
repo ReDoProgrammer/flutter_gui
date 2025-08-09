df023b35798dd23ac6d2ca9fdfb534dc17e7a09c
ReDo Programmer <redoprogrammer@gmail.com>
2025-08-09 14:31:37 +0700

    override toString function để xuất thông tin ds bài hát nhận được

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:main_gui/data/repository/repository.dart';
 
-void main() {}
+void main() async{
+  var respository = DefaultRepository();
+  var songs = await respository.loadData();
+  if(songs != null){
+    for(var song in songs){
+      debugPrint(song.toString());
+    }
+  }
+
+}
 
 class MusicApp extends StatelessWidget {
   const MusicApp({super.key});
