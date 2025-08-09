180ee2074474c3056b3f06d7dbe6a911d16d2591
ReDo Programmer <redoprogrammer@gmail.com>
2025-08-09 18:01:08 +0700

    thiết kế home tab và các tab liên quan

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:main_gui/data/repository/repository.dart';
+import 'package:main_gui/ui/home/home.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  var res = DefaultRepository();
-  var songs = await res.loadData();
-  if(songs != null){
-    for(var song in songs){
-      debugPrint(song.toString());
-    }
-  }
-
-}
-
-class MusicApp extends StatelessWidget {
-  const MusicApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+void main() => runApp(const MusicApp());
--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,7 +1,70 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:main_gui/ui/discovery/discovery.dart';
+import 'package:main_gui/ui/settings/settings.dart';
+import 'package:main_gui/ui/user/profile.dart';
 
 class MusicApp extends StatelessWidget {
   const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MusicHomePage(),
+    );
+  }
+}
+
+class MusicHomePage extends StatefulWidget {
+  const MusicHomePage({super.key});
+
+  @override
+  State<MusicHomePage> createState() => _MusicHomePageState();
+}
+
+class _MusicHomePageState extends State<MusicHomePage> {
+  final List<Widget> _tabs = [
+    const HomeTab(),
+    const DiscoveryTab(),
+    const SettingsTab(),
+    const ProfileTab(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(middle: Text('Music App')),
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.album),
+              label: 'Discovery',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
+        tabBuilder: (BuildContext context, int index) {
+          return _tabs[index];
+        },
+      ),
+    );
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
