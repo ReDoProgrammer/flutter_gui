6a776706d5d5c5a167a4ff780119f4789f5904ae
ReDo Programmer <redoprogrammer@gmail.com>
2025-08-09 20:31:51 +0700

    hiển thị hình của bài hát dạng hình tròn phù hợp với bề rộng của màn hình

--- lib/ui/playing/playing.dart
+++ lib/ui/playing/playing.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -29,13 +31,32 @@ class NowPlayingPage extends StatefulWidget {
   State<NowPlayingPage> createState() => _NowPlayingPageState();
 }
 
-class _NowPlayingPageState extends State<NowPlayingPage> {
+class _NowPlayingPageState extends State<NowPlayingPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _imageAnimController;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _imageAnimController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 12000),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;// chiều rộng màn hình
+    final screenWidth = MediaQuery
+        .of(
+      context,
+    )
+        .size
+        .width; // chiều rộng màn hình
     const delta = 64;
-    final radius = (screenWidth-64)/2;
-    
+    final radius = (screenWidth - 64) / 2;
+
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('Now Playing'),
@@ -54,12 +75,30 @@ class _NowPlayingPageState extends State<NowPlayingPage> {
               const Text('_ ___ _'),
 
               const SizedBox(height: 48),
-
+              RotationTransition(
+                turns: Tween(
+                  begin: 0.0,
+                  end: 1.0,
+                ).animate(_imageAnimController),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(radius),
+                  child: FadeInImage.assetNetwork(
+                    placeholder: 'assets/images/music_default_icon.png',
+                    image: widget.playingSong.image,
+                    width: screenWidth - delta,
+                    height: screenWidth - delta,
+                    imageErrorBuilder: (context, error, stackTrace) {
+                      return Image.asset('assets/images/music_default_icon.png',
+                          width: screenWidth - delta,
+                          height: screenWidth - delta);
+                    },
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
