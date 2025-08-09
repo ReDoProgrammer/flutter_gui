7bab6bca5ffb593bae7d5487534d438d0ce45cc2
ReDo Programmer <redoprogrammer@gmail.com>
2025-08-09 20:15:45 +0700

    hiển thị tên album của bài hát sử dụng scaffold

--- lib/ui/playing/playing.dart
+++ lib/ui/playing/playing.dart
@@ -11,34 +11,47 @@ class NowPlaying extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  NowPlayingPage(songs: songs,playingSong: playingSong);
+    return NowPlayingPage(songs: songs, playingSong: playingSong);
   }
 }
 
 class NowPlayingPage extends StatefulWidget {
-  const NowPlayingPage({super.key,required this.songs,required this.playingSong});
+  const NowPlayingPage({
+    super.key,
+    required this.songs,
+    required this.playingSong,
+  });
+
   final Song playingSong;
   final List<Song> songs;
+
   @override
   State<NowPlayingPage> createState() => _NowPlayingPageState();
 }
 
 class _NowPlayingPageState extends State<NowPlayingPage> {
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('Now Playing'),
-        trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.more_horiz)),// nút 3 chấm theo chiều ngang
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.more_horiz),
+        ), // nút 3 chấm theo chiều ngang
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(widget.playingSong.album,
-            )
-          ],
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(widget.playingSong.album),
+              const SizedBox(height: 16), // khoảng trắng
+              const Text('_ ___ _'),
+
+              const SizedBox(height: 48),
+            ],
+          ),
         ),
       ),
     );
