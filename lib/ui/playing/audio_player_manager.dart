8c5b3f04366a2aba0fcf0c5cd6b8ac545a6d7fa4
ReDo Programmer <redoprogrammer@gmail.com>
2025-08-09 21:33:10 +0700

    tạo class để custom cho media button

--- lib/ui/playing/audio_player_manager.dart
+++ lib/ui/playing/audio_player_manager.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -32,4 +34,34 @@ class DurationState {
   final Duration progress;
   final Duration buffered;
   final Duration? total;
+}
+
+class MediaButtonControl extends StatefulWidget {
+  const MediaButtonControl({
+    super.key,
+    required this.function,
+    required this.icon,
+    required this.color,
+    required this.size,
+  });
+
+  final void Function()? function;
+  final IconData icon;
+  final double? size;
+  final Color? color;
+
+  @override
+  State<StatefulWidget> createState() => _MediaButtonControlState();
+}
+
+class _MediaButtonControlState extends State<MediaButtonControl> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: widget.function,
+      icon: Icon(widget.icon),
+      iconSize: widget.size,
+      color: widget.color??Theme.of(context).colorScheme.primary,
+    );
+  }
 }
